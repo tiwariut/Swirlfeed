1be6f9ba3d07eb053238cfc7af49ee72424521ea
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-22 17:53:16 +0530

    Create login form

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php  
-$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
-
-if(mysqli_connect_errno()) 
-{
-	echo "Failed to connect: " . mysqli_connect_errno();
-}
-
-$query = mysqli_query($con, "INSERT INTO test VALUES ('2', 'Optimus Prime')");
+require 'config/config.php';
 
 ?>
 
