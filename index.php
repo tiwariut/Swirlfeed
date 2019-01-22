963d69e6ed2283ada92a78af98822b07d5b37bfa
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-22 17:51:43 +0530

    Create register form

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<!DOCTYPE html>
+<?php  
+$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+
+if(mysqli_connect_errno()) 
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+$query = mysqli_query($con, "INSERT INTO test VALUES ('2', 'Optimus Prime')");
+
+?>
+
 <html>
 <head>
-    <title>Welcome to Swirlfeed</title>
+	<title>Welcome to Swirlfeed</title>
 </head>
 <body>
-<h1>Home</h1>
+	Hello Reece!!!!!
 </body>
 </html>
