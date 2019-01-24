a28081bc3944fb64082067e073e4ad1a62743014
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-24 13:24:48 +0530

    Show posts on index page

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -10,11 +10,17 @@ class Post
         $this->user_obj = new User($con, $user);
     }
 
-    public function submitPost($body)
+    public function submitPost($title, $image_url, $body)
     {
+        $title = strip_tags($title);
+        $title = mysqli_real_escape_string($this->con, $title);
+
+        $image_url = strip_tags($image_url);
+        $image_url = mysqli_real_escape_string($this->con, $image_url);
+
         $body = strip_tags($body); //removes html tags
         $body = mysqli_real_escape_string($this->con, $body);
-        $check_empty = preg_replace('/\s+/', '', $body); //Deltes all spaces
+        $check_empty = preg_replace('/\s+/', '', $body); //Deletes all spaces
 
         if ($check_empty != "") {
 
@@ -25,7 +31,7 @@ class Post
             $added_by = $this->user_obj->getUsername();
 
             //insert post
-            $query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+            $query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$title', '$image_url', '$body', '$added_by', '$date_added', 'no', 'no', '0')");
             $returned_id = mysqli_insert_id($this->con);
 
             //Insert notification
@@ -35,17 +41,119 @@ class Post
             $num_posts++;
             $update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
 
-            //insert post
-            $query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$date_added', 'no', 'no', '0')");
-            $returned_id = mysqli_insert_id($this->con);
-
-            //Update post count for user
-            $num_posts = $this->user_obj->getNumPosts();
-            $num_posts++;
-            $update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
-
         }
     }
+
+    public function loadPosts(){
+        $str = ""; //String to return
+        $data = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
+
+        while($row = mysqli_fetch_array($data)){
+            $id = $row['id'];
+            $title = $row['title'];
+            $image_url = $row['image_url'];
+            $added_by = $row['added_by'];
+            $date_time = $row['date_added'];
+
+            //Check if user who posted, has their account closed
+            $added_by_obj = new User($this->con, $added_by);
+            if($added_by_obj->isClosed()) {
+                continue;
+            }
+
+            $user_details_query = mysqli_query($this->con, "SELECT first_name, last_name, profile_pic FROM users WHERE username='$added_by'");
+            $user_row = mysqli_fetch_array($user_details_query);
+            $body = $user_row['body']; //Testing
+            $first_name = $user_row['first_name'];
+            $last_name = $user_row['last_name'];
+            $profile_pic = $user_row['profile_pic'];
+
+            //Timeframe
+            $date_time_now = date("Y-m-d H:i:s");
+            $start_date = new DateTime($date_time); //Time of post
+            $end_date = new DateTime($date_time_now); //Current time
+            $interval = $start_date->diff($end_date); //Difference between dates
+            if($interval->y >= 1) {
+                if($interval == 1)
+                    $time_message = $interval->y . " year ago"; //1 year ago
+                else
+                    $time_message = $interval->y . " years ago"; //1+ year ago
+            }
+            else if ($interval-> m >= 1) {
+                if($interval->d == 0) {
+                    $days = " ago";
+                }
+                else if($interval->d == 1) {
+                    $days = $interval->d . " day ago";
+                }
+                else {
+                    $days = $interval->d . " days ago";
+                }
+
+
+                if($interval->m == 1) {
+                    $time_message = $interval->m . " month". $days;
+                }
+                else {
+                    $time_message = $interval->m . " months". $days;
+                }
+
+            }
+            else if($interval->d >= 1) {
+                if($interval->d == 1) {
+                    $time_message = "Yesterday";
+                }
+                else {
+                    $time_message = $interval->d . " days ago";
+                }
+            }
+            else if($interval->h >= 1) {
+                if($interval->h == 1) {
+                    $time_message = $interval->h . " hour ago";
+                }
+                else {
+                    $time_message = $interval->h . " hours ago";
+                }
+            }
+            else if($interval->i >= 1) {
+                if($interval->i == 1) {
+                    $time_message = $interval->i . " minute ago";
+                }
+                else {
+                    $time_message = $interval->i . " minutes ago";
+                }
+            }
+            else {
+                if($interval->s < 30) {
+                    $time_message = "Just now";
+                }
+                else {
+                    $time_message = $interval->s . " seconds ago";
+                }
+            }
+
+
+            $str .= "<div class='status_post'>
+								<div class='post_profile_pic'>
+									<img src='$profile_pic' width='50'>
+								</div>
+
+								<div class='posted_by' style='color:#ACACAC;'>
+									<a href='$added_by'> $first_name $last_name </a>  &nbsp;&nbsp;&nbsp;&nbsp;$time_message
+								</div>
+								<div id='post_body'>
+									$body
+									<br>
+								</div>
+
+							</div>";
+
+        }
+
+        echo $str ;
+
+    }
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -12,38 +12,50 @@ include("includes/classes/Post.php");
 ?>
 
 
-
-
 <div class="container">
     <div class="row">
         <div class="col-md-4">
             <div class="card">
                 <a href="<?php echo $userLoggedIn; ?>">
-                <h3 class="card-title text-center" style="margin-top: 0.75rem;"><?php echo $user['first_name'] . " " . $user['last_name']; ?></h3>
-                <img class="card-img-top" src="<?php echo $user['profile_pic']; ?>" alt="Card image cap"></a>
+                    <h3 class="card-title text-center" style="margin-top: 0.75rem;"><?php echo $user['first_name'] . " " . $user['last_name']; ?></h3>
+                    <img class="card-img-top" src="<?php echo $user['profile_pic']; ?>" alt="Card image cap"></a>
                 <div class="card-body">
                     <div class="caption text-center">Posts: <?php echo $user['num_posts']; ?> Likes: <?php echo $user['num_likes']; ?></div>
                 </div>
             </div>
 
         </div>
-        <div class="col-md-8">
-            <div class="card">
-                <h2 class="text-center" style="padding: 20px;"><%= user.username %>'s Posts</h2>
-                <% posts.forEach(function(post){ %>
-                <div class="rounded mx-auto d-block card border-secondary bg-light mb-3" style="width: 90%;">
-                    <img class="card-img-top" src="<%= post.image %>">
-                    <div class="card-body">
-                        <h5 class="card-title"><%= post.title %></h5>
-                        <p class="card-text"><%- post.body.substring(0, 200) %>...</p>
-                        <p class="card-text"><small class="text-muted"><%= moment(post.createdAt).fromNow() %></small></p>
-                        <a href="/posts/<%= post._id %>" class="btn btn-sm btn-outline-dark">Read</a>
-                    </div>
-                </div>
-                <% }) %>
-            </div>
-        </div>
+
+        <?php
+
+        $post = new Post($con, $userLoggedIn);
+        $post->loadPosts();
+
+        ?>
+
+
+
+
+
+<!--        <div class="col-md-8">-->
+<!--            <div class="card">-->
+<!--                <h2 class="text-center" style="padding: 20px;"><%= user.username %>'s Posts</h2>-->
+<!--                <% posts.forEach(function(post){ %>-->
+<!--                <div class="rounded mx-auto d-block card border-secondary bg-light mb-3" style="width: 90%;">-->
+<!--                    <img class="card-img-top" src="<%= post.image %>">-->
+<!--                    <div class="card-body">-->
+<!--                        <h5 class="card-title"><%= post.title %></h5>-->
+<!--                        <p class="card-text"><%- post.body.substring(0, 200) %>...</p>-->
+<!--                        <p class="card-text"><small class="text-muted"><%= moment(post.createdAt).fromNow() %></small></p>-->
+<!--                        <a href="/posts/<%= post._id %>" class="btn btn-sm btn-outline-dark">Read</a>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <% }) %>-->
+<!--            </div>-->
+<!--        </div>-->
     </div>
+</div>
+
 </div>
 
 
