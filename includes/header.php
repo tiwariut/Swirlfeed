bcf28c47866a94b54b9881e9daa1c687f893a0f2
Utkarsh Tiwari <tiwariutkarsh007@gamil.com>
2019-01-25 16:41:42 +0530

    Finishing up

--- includes/header.php
+++ includes/header.php
@@ -28,7 +28,7 @@ if(isset($_SESSION['username'])){
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light navbar-dark bg-dark">
     <a class="navbar-brand" href="index.php"><i class="fab fa-readme"></i> Swirlfeed</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
--- index.php
+++ index.php
@@ -6,24 +6,6 @@ include("includes/classes/Post.php");
 
 ?>
 
-<!---->
-<!--<div class="container">-->
-<!--    <div class="row">-->
-<!--        <div class="col-md-4">-->
-<!--            <div class="card">-->
-<!--                <a href="--><?php //echo $userLoggedIn; ?><!--">-->
-<!--                    <h3 class="card-title text-center" style="margin-top: 0.75rem;">--><?php //echo $user['first_name'] . " " . $user['last_name']; ?><!--</h3>-->
-<!--                    <img class="card-img-top" src="--><?php //echo $user['profile_pic']; ?><!--" alt="Card image cap"></a>-->
-<!--                <div class="card-body">-->
-<!--                    <div class="caption text-center">Posts: --><?php //echo $user['num_posts']; ?><!-- Likes: --><?php //echo $user['num_likes']; ?><!--</div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!---->
-<!--        </div>-->
-<!---->
-<!---->
-<!--    </div>-->
-
     <?php
 
 
@@ -31,11 +13,6 @@ include("includes/classes/Post.php");
     $post->loadPosts();
 
     ?>
-</div>
-
-
-
-</div>
 
 
 </body>
